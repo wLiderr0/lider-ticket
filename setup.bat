4d8914facd4aa35648412b66811835f315739352
wLiderr0 <142577384+wLiderr0@users.noreply.github.com>
2025-06-02 22:06:58 +0300

    Add files via upload

---
+++ setup.bat
@@ -0,0 +1,9 @@
+color 1
+cls
+:s
+npm install
+echo "Kurulum Tamamlandı"
+start.bat
+```
+start.bat
+```REM Path: start.bat
---
+++ start.bat
@@ -0,0 +1,8 @@
+echo off
+title wLiderr0 Ticket Bot
+color 3
+cls
+:a
+node index.js
+goto a
+pause
